ed180a56837a5efc90eca668fc0c6ab5aec0306e
Zuzmen <wlublinski@gmail.com>
2017-06-23 00:48:56 +0200

    Warsztat1_Sit_on_Chair

---
+++ 1_Sit_on_chair/script.js
@@ -0,0 +1,11 @@
+console.log("Vladyslav");
+/*
+"Hello World";
+"Hello World"
+
+Hello World;
+VM153:1 Uncaught SyntaxError: Unexpected identifier
+
+"Hello World;
+VM154:1 Uncaught SyntaxError: Invalid or unexpected token
+*/
